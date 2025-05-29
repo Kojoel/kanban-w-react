90b9eb2e8f4d75a3f33d651bd45ef936645cae36
Kojoel <nanaabruquah1s@gmail.com>
2025-05-29 16:29:18 +0000

    feat: implement active board functionality

--- src/store/state.ts
+++ src/store/state.ts
@@ -4,9 +4,15 @@ import type { Board, Column } from '../interfaces/board';
 interface BoardState {
     boards: Board[];
     setBoards: (boards: Board[]) => void;
+    
+    activeBoard: string;
+    setActiveBoard: (board: string) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
     boards: [],
     setBoards: (boards) => set({ boards }),
+    
+    activeBoard: '',
+    setActiveBoard: (board: string) => set({ activeBoard: board }),
 }))
